dc8ad2c12d36569748e5d21e9663be2ec1c8def9
jaebum7396@naver.com <jaebum7396@naver.com>
2023-11-07 20:04:07 +0900

    init. 공통 서브모듈 첫커밋

---
+++ src/main/java/com/common/MyInterceptor.java
@@ -0,0 +1,70 @@
+package com.common;
+
+import lombok.extern.slf4j.Slf4j;
+import com.common.utils.CachedBodyHttpServletWrapper;
+import com.common.utils.CommonUtils;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+@Slf4j
+public class MyInterceptor implements HandlerInterceptor{
+	private final String GATEWAY_URI;
+	private final String ACTIVE_PROFILE;
+	public MyInterceptor(String GATEWAY_URI, String ACTIVE_PROFILE) {
+		this.GATEWAY_URI = GATEWAY_URI;
+		this.ACTIVE_PROFILE = ACTIVE_PROFILE;
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		log.info("===============================================");
+        log.info("==================== BEGIN ====================");
+        Enumeration eHeader = request.getHeaderNames();
+    	while (eHeader.hasMoreElements()) {
+	    	String key = (String)eHeader.nextElement();
+	    	String value = request.getHeader(key);
+	    	log.info("key : " + key + " ===> value : " + value);
+	    }
+
+		String requestUri = request.getHeader("x-forwarded-host");
+
+		System.out.println("ACTIVE_PROFILE : " + ACTIVE_PROFILE);
+		System.out.println("GATEWAY_URI : " + GATEWAY_URI);
+		if(ACTIVE_PROFILE.equals("local")) {
+			log.info("로컬에서 실행중입니다.");
+			//GATEWAY_URI = "http://localhost:8080";
+		}else{
+			System.out.println("요청_URI : " + requestUri);
+			if(("".equals(requestUri)||requestUri == null||!requestUri.equals(GATEWAY_URI))) {
+				throw new BadCredentialsException("잘못된 접근입니다.");
+			}
+		}
+
+		// 요청 본문을 읽고 출력합니다.
+		final CachedBodyHttpServletWrapper cachingRequest = (CachedBodyHttpServletWrapper) request;
+		String requestBodyStr = "";
+		requestBodyStr = CommonUtils.readRequestBody(cachingRequest);
+		System.out.println("requestBody : " + requestBodyStr); // 요청 본문 내용을 출력합니다.
+
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		log.info("==================== END ======================");
+        log.info("===============================================");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)	throws Exception {
+		// TODO Auto-generated method stub
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+	
+}
---
+++ src/main/java/com/common/configuration/RequestConfig.java
@@ -0,0 +1,31 @@
+package com.common.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import com.common.utils.CachedBodyHttpServletWrapper;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestConfig implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        CachedBodyHttpServletWrapper cachedBodyHttpServletWrapper = new CachedBodyHttpServletWrapper(httpRequest);
+        chain.doFilter(cachedBodyHttpServletWrapper, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
