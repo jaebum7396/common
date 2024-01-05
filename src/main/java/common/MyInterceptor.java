af52129d50dba93de4c0b3b667309c9e3df8b109
주재범 <jaebum7396@naver.com>
2024-01-05 12:21:28 +0900

    refactor. 스프링 클라우드 종속성 관리하도록

--- src/main/java/common/MyInterceptor.java
+++ src/main/java/common/MyInterceptor.java
@@ -1,6 +1,11 @@
 package common;
 
+import common.utils.CachedBodyHttpServletWrapper;
+import common.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,26 +15,27 @@ import java.util.Enumeration;
 
 @Slf4j
 public class MyInterceptor implements HandlerInterceptor{
-	//private final String GATEWAY_URI;
+	private Logger logger = LoggerFactory.getLogger(MyInterceptor.class);
+	private final String GATEWAY_URI;
 	private final String ACTIVE_PROFILE;
 	public MyInterceptor(
 			String ACTIVE_PROFILE
-			//, String GATEWAY_URI
+			, String GATEWAY_URI
 	) {
 		this.ACTIVE_PROFILE = ACTIVE_PROFILE;
-		//this.GATEWAY_URI = GATEWAY_URI;
+		this.GATEWAY_URI = GATEWAY_URI;
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		log.info("===============================================");
-        log.info("==================== BEGIN ====================");
-        Enumeration eHeader = request.getHeaderNames();
-    	while (eHeader.hasMoreElements()) {
-	    	String key = (String)eHeader.nextElement();
-	    	String value = request.getHeader(key);
-	    	log.info("key : " + key + " ===> value : " + value);
-	    }
+		log.info("==================== BEGIN ====================");
+		Enumeration eHeader = request.getHeaderNames();
+		while (eHeader.hasMoreElements()) {
+			String key = (String)eHeader.nextElement();
+			String value = request.getHeader(key);
+			log.info("key : " + key + " ===> value : " + value);
+		}
 
 		String requestUri = request.getHeader("x-forwarded-host");
 
@@ -38,18 +44,18 @@ public class MyInterceptor implements HandlerInterceptor{
 			log.info("로컬에서 실행중입니다.");
 			//GATEWAY_URI = "http://localhost:8080";
 		}
-		/*else{
+		else{
 			System.out.println("요청_URI : " + requestUri);
 			if(("".equals(requestUri)||requestUri == null||!requestUri.equals(GATEWAY_URI))) {
 				throw new BadCredentialsException("잘못된 접근입니다.");
 			}
-		}*/
+		}
 
 		// 요청 본문을 읽고 출력합니다.
-		/*final CachedBodyHttpServletWrapper cachingRequest = (CachedBodyHttpServletWrapper) request;
+		final CachedBodyHttpServletWrapper cachingRequest = (CachedBodyHttpServletWrapper) request;
 		String requestBodyStr = "";
 		requestBodyStr = CommonUtils.readRequestBody(cachingRequest);
-		System.out.println("requestBody : " + requestBodyStr); // 요청 본문 내용을 출력합니다.*/
+		System.out.println("requestBody : " + requestBodyStr); // 요청 본문 내용을 출력합니다.
 
 		return HandlerInterceptor.super.preHandle(request, response, handler);
 	}
@@ -57,7 +63,7 @@ public class MyInterceptor implements HandlerInterceptor{
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 		log.info("==================== END ======================");
-        log.info("===============================================");
+		log.info("===============================================");
 		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
 	}
 
@@ -66,5 +72,5 @@ public class MyInterceptor implements HandlerInterceptor{
 		// TODO Auto-generated method stub
 		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
 	}
-	
+
 }
